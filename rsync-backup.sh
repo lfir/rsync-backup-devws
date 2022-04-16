25058561114bc334184e80b7d1481c09060f3a46
Asta86 <psljp@protonmail.com>
2022-04-16 18:25:59 -0300

    Since error log and last run files are not deleted and recreated there is no need to update the owner every time the script is run

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -11,7 +11,6 @@ lastrunfn='last-backup-date.txt'
 errlogfn='error-log.txt'
 date -I > "$lastrunfn"
 grep 'rsync error' "$logfn" >> "$errlogfn"
-chown "$username" "$lastrunfn" "$errlogfn"
 cp "$lastrunfn" "$errlogfn" "$dest/$username/Devel/rsync-backup-devws"
 rm "$logfn"
 
