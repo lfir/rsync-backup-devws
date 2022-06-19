f8d8bf8fc76dede3de3dd325323d3ff1315610a4
Asta86 <psljp@protonmail.com>
2022-06-19 20:52:49 -0300

    Add comments

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Owner of the home directory being backed up
 username='asta86'
+# Expected home and root mountpoints
 targets=("/mnt/h/$username" '/mnt/r/etc')
 dest='/media/ubuntu/bkp'
 logfn='log.txt'
@@ -9,8 +11,11 @@ rsync -Aah --info=progress2 --stats --delete --log-file="$logfn" --exclude-from=
 
 lastrunfn='last-backup-date.txt'
 errlogfn='error-log.txt'
+# Save the date of the current run
 date -I > "$lastrunfn"
+# Keep only rsync errors in the final log
 grep 'rsync error' "$logfn" >> "$errlogfn"
+# Copy files created after rsync ran to the destination
 cp "$lastrunfn" "$errlogfn" "$dest/$username/Devel/rsync-backup-devws"
 rm "$logfn"
 
